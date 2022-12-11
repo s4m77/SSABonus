17695b97b6af2603cbbbc40e04caf03fb1a52159
eduard <e.vonbothmer@student.maastrichtuniversity.nl>
2022-12-11 20:54:16 +0100

    csv

--- src/Simulation.java
+++ src/Simulation.java
@@ -5,7 +5,8 @@
  */
 
 package src;
-
+import java.io.FileWriter;
+import java.io.IOException;
 public class Simulation {
 
 	public CEventList list;
@@ -34,6 +35,41 @@ public class Simulation {
 
 		// start the eventlist
 		l.start((24*10+7)*60); // 2000 is maximum time
-	}
 
+		// to  generate data for matlab
+		int size = si.getNumbers().length;
+        double[][] arr = new double[size/3][5];
+
+        int idx = 0;
+        for (int i = 0; i < si.getNumbers().length; i+=3) {
+            arr[idx][0] = si.getNumbers()[i];
+            arr[idx][1] = si.getTypes()[i];
+            arr[idx][2] = si.getTimes()[i]; // creation time
+            arr[idx][3] = si.getTimes()[i+1]; // start production
+            arr[idx][4] = si.getTimes()[i+2]; // end prooduction
+            idx++;
+        }
+
+		for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        try (FileWriter writer = new FileWriter("JavaOut.csv")) {
+            for (int j = 0; j < arr.length; j++) {
+                String row = String.valueOf(arr[j][0])+", "+String.valueOf(arr[j][1])+", "
+                +String.valueOf(arr[j][2])+", "+String.valueOf(arr[j][3])+", "+String.valueOf(arr[j][4]);
+                writer.append(row);
+                writer.append("\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
+
+
--- src/Sink.java
+++ src/Sink.java
@@ -13,6 +13,7 @@ public class Sink implements ProductAcceptor
 	/** All properties of products are kept */
 	private ArrayList<Integer> numbers;
 	private ArrayList<Double> times;
+	private ArrayList<Integer> types;
 	private ArrayList<String> events;
 	private ArrayList<String> stations;
 	/** Counter to number products */
@@ -26,6 +27,7 @@ public class Sink implements ProductAcceptor
 	public Sink(String n)
 	{
 		name = n;
+		types = new ArrayList<>();
 		products = new ArrayList<>();
 		numbers = new ArrayList<>();
 		times = new ArrayList<>();
@@ -37,14 +39,24 @@ public class Sink implements ProductAcceptor
         @Override
 	public boolean giveProduct(Product p)
 	{
+		Patient patient = (Patient) p;
 		number++;
 		products.add(p);
+		int typeNumber = 0;
+		if (patient.getType().equals("a1")){
+			typeNumber = 1;
+		}else if (patient.getType().equals("a2")){
+			typeNumber = 2;
+		}else{
+			typeNumber = 3;
+		}
 		// store stamps
 		ArrayList<Double> t = p.getTimes();
 		ArrayList<String> e = p.getEvents();
 		ArrayList<String> s = p.getStations();
 		for(int i=0;i<t.size();i++)
 		{
+			types.add(typeNumber);
 			numbers.add(number);
 			times.add(t.get(i));
 			events.add(e.get(i));
@@ -52,6 +64,17 @@ public class Sink implements ProductAcceptor
 		}
 		return true;
 	}
+
+	public int[] getTypes()
+    {
+        types.trimToSize();
+        int[] tmp = new int[types.size()];
+        for (int i=0; i < types.size(); i++)
+        {
+            tmp[i] = (types.get(i)).intValue();
+        }
+        return tmp;
+    }
 	
 	public int[] getNumbers()
 	{
